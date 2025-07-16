5ef57596681d028a54d58aca096b396be8bb03ca
mtran2424 <mtran2424@gmail.com>
2025-07-16 16:11:49 -0400

    Added tabs to dashboard

--- components/dashboard/Dashboard.tsx
+++ components/dashboard/Dashboard.tsx
@@ -2,12 +2,14 @@
 import { useUser } from "@clerk/nextjs";
 import { motion } from "framer-motion";
 import { redirect } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ProductsSpreadsheet from "./ProductsSpreadsheet";
 import { Announcement, Product } from "@/components/global.utils";
 import AnnouncementsSpreadsheet from "./AnnouncementsSpreadsheet";
 
 const Dashboard = ({ products, announcements }: { products: Product[], announcements: Announcement[] }) => {
+  const [tab, setTab] = useState<'products' | 'announcements'>('products');
+
   // Admin check
   const { isSignedIn } = useUser();
 
@@ -33,9 +35,36 @@ const Dashboard = ({ products, announcements }: { products: Product[], announcem
           Administrative Dashboard
         </h1>
 
+        {/* Tab Buttons */}
+        <div className="flex flex-row justify-center relative w-full bg-zinc-200">
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            className={`px-6 py-3 text-md z-10 ${tab === 'products'
+              ? 'bg-white text-zinc-900'
+              : 'text-blue-500 hover:text-zinc-700 bg-zinc-200'
+              } rounded-t-xl shadow-t-xl border-t-1 border-r-1 border-zinc-300`}
+            onClick={() => setTab('products')}
+            disabled={tab === 'products'}
+          >
+            Products
+          </motion.button>
+
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            className={`px-6 py-3 text-md z-10 ${tab === 'announcements'
+              ? 'bg-white text-zinc-900'
+              : 'text-blue-500 hover:text-zinc-700 bg-zinc-200'
+              } rounded-t-xl shadow-t-xl border-t-1 border-r-1 border-zinc-300`}
+            onClick={() => setTab('announcements')}
+            disabled={tab === 'announcements'}
+          >
+            Announcements
+          </motion.button>
+        </div>
+
         {/* Spreadsheets */}
-        <ProductsSpreadsheet initialProducts={products} />
-        <AnnouncementsSpreadsheet initialAnnouncements={announcements} />
+        {tab === 'products' && <ProductsSpreadsheet initialProducts={products} />}
+        {tab === 'announcements' && <AnnouncementsSpreadsheet initialAnnouncements={announcements} />}
       </div>
     </motion.div>
   );
