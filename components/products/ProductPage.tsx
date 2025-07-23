68e2f842b863a4e3b4aa0eda6c9b1e65ce2fa875
mtran2424 <mtran2424@gmail.com>
2025-07-23 12:50:17 -0400

    Prep for better quality images

--- components/products/ProductPage.tsx
+++ components/products/ProductPage.tsx
@@ -67,7 +67,7 @@ const ProductPage = ({ products }: { products: Product[] }) => {
           {/* Image */}
           <div className="flex flex-col w-full h-full items-center font-serif text-zinc-400 gap-2">
             {product && product.imageUrl ?
-              <div className="relative w-full h-full min-h-[600px] min-w-[600px]">
+              <div className="relative w-full h-full min-h-[800px] min-w-[800px]">
                 <Image
                   src={product.imageUrl}
                   alt={product.name + " image"}
