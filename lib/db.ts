f4b094c4a40a5d3a5a2c41b330154e5bb3a88cd9
mtran2424 <mtran2424@gmail.com>
2026-01-09 18:42:28 -0500

    Correct prisma client declaration

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,18 @@
 import { PrismaClient } from "@/lib/generated/prisma";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+const pool = new Pool({ 
+  connectionString: process.env.DATABASE_URL 
+});
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db
+const adapter = new PrismaPg(pool);
+
+export const db = globalThis.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
